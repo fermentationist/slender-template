2e819cf4a685f7a49dad8f3ea8759c4d8b73d85f
Dennis Hodges <fermentationist@gmail.com>
2019-09-23 22:39:27 -0500

    Changed part of welcome message passed as props.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import SvelteApp from "./App.svelte";
 import path from "path";
+
 const App = new SvelteApp({
     target: document.body,
     props: { 
-        content: `This text was passed as props to the App.svelte component.`
+        content: `This text was passed as props to the component at '/src/App.svelte', from within '/src/index.js'.`
     }
 });
 
