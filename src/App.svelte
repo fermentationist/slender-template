a8c7069c04f75c2975efe4800a91971e229e2141
Dennis Hodges <fermentationist@gmail.com>
2019-09-24 01:48:00 -0500

    Added some styles to App.svelte and changed welcome message.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 <script>
-    export let content = "NO PROPS PASSED.";//default props
+    export let welcomeMessage = "NO PROPS PASSED.";//default props
     
 </script>
 
 <style>
+    html {
+		box-sizing: border-box;
+		scroll-behavior: smooth;
+		outline: none;
+	}
+	*, *:before, *:after {
+		box-sizing: inherit;
+	}
+	body {
+		padding: 0;
+		margin: 0;
+		text-rendering: optimizeLegibility;
+	}
+	a {
+		text-decoration: none;
+		color: inherit;
+    }
+    
     .thinner-welcome-message {
         text-rendering: optimizeLegibility;
         background-color: rgb(20, 22, 18);
@@ -14,7 +32,7 @@
         font-size: 1.5em;
         padding: 12vh 24vw;
         letter-spacing: 0.25em;
-        line-height: 1.8em;
+        line-height: 2em;
     }
     p {
         max-width: 50vw;
@@ -25,11 +43,14 @@
     .italic {
         font-style: italic;
     }
+    .small-caps {
+        font-variant: small-caps;
+        font-size: 1.33em;
+    }
 </style>
 
 <div class="thinner-welcome-message">
     <h2>Hey.</h2>
-    <p>App.svelte component has been loaded successfully.</p>
-    <p class="italic">{content}</p>
-    <p>You should be good-to-go. Edit App.svelte to make changes.</p>
+    <p>This is a Svelte app. More on Svelte at <a href="https://svelte.dev" class="small-caps">svelte.dev</a>.</p>
+    <p><span>You should be good-to-go. </span><span class="italic">{welcomeMessage}</span></p>
 </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import SvelteApp from "./App.svelte";
-import path from "path";
 
 const App = new SvelteApp({
     target: document.body,
     props: { 
-        content: `This text was passed as props to the component at '/src/App.svelte', from within '/src/index.js'.`
+        welcomeMessage: `Edit "/src/App.svelte" and "/src/index.js" to make changes.`
     }
 });
 
